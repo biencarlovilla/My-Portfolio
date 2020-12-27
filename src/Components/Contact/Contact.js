c3f1b0a76e124e6b0fdee414e46760532e50038b
Biencarlo Villa <vbiencarlo_29@yahoo.com>
2020-12-26 16:44:33 -0800

    Added basic personal information in the contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -11,12 +11,22 @@ const Contact = () => {
             <h1><i class="chess icon"></i>Contact</h1>
             <div class="block"></div>
 
-            
-                <div class="row">
-                <a href="mailto:biencarlovilla@gmail.com"><input class="button-primary" type="submit" value="Let's Connect!" /></a>
-                </div>
-                
-                <div>
+            <div class="row">
+
+            <div class="six columns">
+                <i class="github icon large"></i>Biencarlo Villa
+                <br />
+                <i class="github icon large"></i>Bay Area, CA
+                <br />
+                <i class="linkedin icon large"></i>biencarlovilla@gmail.com
+                <br />
+                <i class="instagram icon large"></i>+1 (650)-201-6337
+            </div>
+               
+               
+                <div class="six columns">
+                <a href="mailto:biencarlovilla@gmail.com"><input class="button-primary" type="submit" value="Let's get to work!" /></a>
+                <br />
                 <a href="https://github.com/biencarlovilla"><i class="github icon large"></i></a>
         
                 <a href="https://www.linkedin.com/in/biencarlo-villa/"><i class="linkedin icon large"></i></a>
@@ -24,7 +34,7 @@ const Contact = () => {
                 <a href="https://www.instagram.com/biencarlovilla/"><i class="instagram icon large"></i></a>
                 </div>
 
-
+            </div>
         </div>
      </section>
     </React.Fragment>
